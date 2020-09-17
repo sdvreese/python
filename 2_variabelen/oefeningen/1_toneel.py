a05e862394b35faeed035c706098ad826879a245
Sabine De Vreese <sabine.devreese@hogent.be>
2020-09-17 10:14:46 +0200

    oefeningen variabelen

---
+++ 2_variabelen/oefeningen/1_toneel.py
@@ -0,0 +1,5 @@
+"""
+Schrijf een programma dat de inkomsten voor 1 avond berekent voor een toneelvoorstelling. 
+Er zijn 3 soorten plaatsen: A (€ 25), B(€ 20) of C(€ 15). 
+De gebruiker moet ingeven hoeveel mensen betaalden voor klasse A, B of C.
+"""
