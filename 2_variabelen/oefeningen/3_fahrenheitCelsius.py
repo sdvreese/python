6abf207164f4461193f0a82b6aa131fed1982c5e
Sabine De Vreese <sabine.devreese@hogent.be>
2020-09-17 10:23:22 +0200

    variabelen oefeningen

---
+++ 2_variabelen/oefeningen/3_fahrenheitCelsius.py
@@ -0,0 +1,5 @@
+"""
+Schrijf een programma dat de temperatuur in graden Fahrenheit converteert naar het equivalent 
+in graden Celsius.
+De formule is: c = (f – 32) * 5 / 9
+"""
---
+++ 2_variabelen/oefeningen/5_electriciteit.py
@@ -0,0 +1,8 @@
+"""
+Ontwerp een programma waarmee de gebruiker het bedrag kan berekenen dat hij moet betalen voor zijn elektriciteit. Dit bedrag wordt als volgt samengesteld:
+· Enerzijds is er de hoeveelheid elektriciteit die je verbruikt. 
+Deze hoeveelheid (in kWH) kan door de gebruiker ingegeven worden. 
+Één kWH kost € 0.15
+· Anderzijds wordt er altijd een vaste abonnementsprijs van 18 euro aangerekend.
+· Op het abonnementsgeld en het verbruik is daarenboven nog 21% BTW verschuldigd.
+"""
